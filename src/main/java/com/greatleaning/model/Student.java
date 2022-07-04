76043aecda9d73a9ca66f2177b003793ffff9442
Joshua Ninan Valson <joshuavalson@gmail.com>
2022-07-04 21:47:40 +0530

    checkin

---
+++ src/main/java/com/greatleaning/model/Student.java
@@ -0,0 +1,80 @@
+package com.greatleaning.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String course;
+
+	private String country;
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String firstName, String lastName, String course, String country) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.course = course;
+		this.country = country;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", course=" + course
+				+ ", country=" + country + "]";
+	}
+
+}
---
+++ src/main/java/com/greatlearning/controller/StudentController.java
@@ -0,0 +1,81 @@
+package com.greatlearning.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.greatleaning.model.Student;
+import com.greatlearning.service.StudentService;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+	@Autowired
+	StudentService studentServiceImpl;
+
+	@RequestMapping("/list")
+	public String listStudent(Model model) {
+		List<Student> students = studentServiceImpl.findAll();
+
+		model.addAttribute("students", students);
+		return "listStudent";
+	}
+
+	@RequestMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+
+		Student student = new Student();
+
+		model.addAttribute("student", student);
+
+		return "addStudent";
+
+	}
+
+	@RequestMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("id") int id, Model model) {
+
+		Student student = studentServiceImpl.findById(id);
+
+		model.addAttribute("student", student);
+
+		return "addStudent";
+	}
+
+	@PostMapping("/save")
+	public String save(@RequestParam("id") int id, @RequestParam("firstName") String firstName,
+			@RequestParam("lastName") String lastName, @RequestParam("course") String course,
+			@RequestParam("country") String country) {
+
+		Student student;
+		if (id != 0) {
+			student = studentServiceImpl.findById(id);
+			student.setFirstName(firstName);
+			student.setLastName(lastName);
+			student.setCourse(course);
+			student.setCountry(country);
+
+		} else {
+			student = new Student(firstName, lastName, course, country);
+		}
+
+		studentServiceImpl.save(student);
+
+		return "redirect:/student/list";
+
+	}
+
+	@RequestMapping("/delete")
+	public String delete(@RequestParam("id") int id) {
+
+		studentServiceImpl.deleteById(id);
+		return "redirect:/student/list";
+	}
+
+}
